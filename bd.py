85f37c596080bda1b89098f1aa09c679f0c043d4
sidorevichAlex <sidorevich19771737@gmail.com>
2020-02-26 14:52:41 +0300

    Proverka new branch

--- bd.py
+++ bd.py
@@ -1,6 +1,5 @@
 import pymysql.cursors
-
-
+from math import sin,pi
 def view(connect):
     with connect.cursor() as cursor:
         cursor.execute("""SELECT * FROM users""")
@@ -10,23 +9,52 @@ def view(connect):
 
 
 def Change(connect, query: str):
-    with connect.cursor() as cursor:
-        cursor.execute("""""" + query + """""")
-    connect.commit()
+    try:
+        with connect.cursor() as cursor:
+            cursor.execute(query)
+        connect.commit()
+    except:
+        pass
 
 
 link = pymysql.connect(host='localhost',
                        user='root',
                        password='root',
-                       db='test',
+                       db='sin',
                        charset='utf8mb4',
                        cursorclass=pymysql.cursors.DictCursor
                        )
 # view(link)
-query = "DELETE FROM users WHERE id > 6520;"
-Change(link, query)
-view(link)
-query = ""
+query = "TRUNCATE `sinus`"
+Change(link,query)
+POV = 1
+for _ in range(POV):
+    x = -100
+    shag = 200 / (2880*3) * POV
+    for hour in range(0,24):
+        time = ""
+        if hour < 10:
+            time += "0" + str(hour) + ":"
+        else:
+            time += str(hour) + ":"
+        for minute in range(0,60):
+            time = time[:3]
+            if minute < 10:
+                time += "0" + str(minute) + ":"
+            else:
+                time += str(minute) + ":"
+            for sec in range(0,60,10):
+                time = time[:6]
+                if sec < 10:
+                    time += "0" + str(sec)
+                else:
+                    time += str(sec)
+                query = "INSERT INTO `sinus` (`y`, `t`) VALUES ('"+str(x*x*0.5)+"', '2020-02-20 "+time+"')"
+                Change(link, query)
+                x += shag
+
+# view(link)
+# query = ""
 # Change(link,query)
-view(link)
+# view(link)
 link.close()
