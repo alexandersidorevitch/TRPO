148fa36129173d6c8d4a5e25d4152a64f28fa2a5
sidorevichAlex <sidorevich19771737@gmail.com>
2020-03-24 20:38:41 +0300

    New Fonts

--- NEW_MAIN.py
+++ NEW_MAIN.py
@@ -1,4 +1,7 @@
 from tkinter import *
+from tkinter import font
+
+from ttkthemes import ThemedTk
 
 
 def callback(event):
@@ -10,27 +13,44 @@ def but(number: int):
     global buttons
     buttons[number]["bg"] = '#333333'
 
+
 def butleave(number: int):
     global buttons
     buttons[number]["bg"] = '#222222'
 
+
+def pow(number, n):
+    otvet = 1
+    for i in range(n):
+        otvet *= number
+    return otvet
+
+
 def press(number: int):
     global buttons
-    print(number)
-    buttons[number]['text'] = str(number + 1)
-root = Tk()
+    buttons[number]['text'] = 'Privet'
+
+
+root = ThemedTk()
+root.set_theme('breeze')
 root.geometry("600x500+300+200")
 listbox1 = Listbox(root, height=10, width=15, selectmode=EXTENDED, bg='#222222', highlightcolor='red', fg="#EEEEEE")
 listbox2 = Listbox(root, height=5, width=15, selectmode=SINGLE)
 button1 = Button(root, text='ok', width=25, height=5, bg='white', fg='red', font='arial 14')
 buttons = []
 q = root.grid()
-for i in range(10):
-    buttons.append(Button(text=str(i), width=10, height=2,bg='#222222',fg = '#EEEEEE',bd=0,activebackground='#333333',activeforeground='#EEEEEE',highlightcolor="black",relief=SUNKEN, command=lambda number = i: press(number)))
-    buttons[i].grid(row = i,column = 1,columnspan=1,ipadx=103,ipady=3)
+fon = list(font.families())
+fon = [fon[50], fon[88], fon[110]]
+for i in range(9):
+    f = font.Font(family=fon[1], size=14)
+    buttons.append(
+        Button(text=str('Стелла'), width=10, height=2, bg='#222222', fg='#EEEEEE', bd=0, activebackground='#333333',
+               activeforeground='#EEEEEE', highlightcolor="black", relief=SUNKEN, wraplength=200,
+               font=f, command=lambda number=i: press(number)))
+    buttons[i].grid(row=i, column=1, columnspan=1, ipadx=103, ipady=3)
     buttons[i].bind("<Enter>", lambda event, number=i: but(number))
     buttons[i].bind("<Leave>", lambda event, number=i: butleave(number))
     # buttons[i].pack()
-i =0
+i = 0
 
 root.mainloop()
---
+++ ввод полей.py
@@ -0,0 +1,37 @@
+from tkinter import font
+from tkinter.ttk import *
+from ttkthemes import ThemedTk
+from sqlite3 import *
+
+# Режим отображение таблиц
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+conn = connect("TRPO.db")  # подлкючение к БД
+
+conn.row_factory = dict_factory
+
+cursor = conn.cursor()
+
+root = ThemedTk()
+root.set_theme('equilux')
+print(font.families())
+# s = Style()
+# print(s.theme_names())
+# s.theme_use('clam')
+root.geometry("600x500+300+200")
+
+frame = Frame(root)
+frame.grid()
+
+listtables = []
+cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+for i in cursor.fetchall():
+    if i['name'] != "sqlite_sequence":
+        listtables.append(i['name'])
+ComboTables =Combobox(frame,values = listtables,height=10)
+# Combobox.set(value = "Таблицы")
+ComboTables.pack(side="left")
+root.mainloop()
